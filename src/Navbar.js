0c72e3c42178031f901fc0079412a2043128e3ac
JamesRonderos <wakeru@gmail.com>
2020-12-19 20:48:51 +0900

    Added Select and material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import { Component } from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import Slider from "rc-slider";
 import 'rc-slider/assets/index.css';
 import "./Navbar.css"
 
 function Navbar(props) {
-    const {colorLevel, changeLevel} = props
+    const {colorLevel, changeLevel, handleFormatChange} = props
+    const [colorFormat, setColorFormat] = useState("hex");
+
+    function handleChange(e) {
+        setColorFormat(e.target.value)
+        handleFormatChange(e.target.value)
+    }
+
     return(
         <header className="Navbar">
             <div className="logo">
@@ -15,6 +25,13 @@ function Navbar(props) {
                 <div className="slider">
                     <Slider defaultValue={colorLevel} min={100} max={900} step={100} onAfterChange={changeLevel}/>
                 </div>
+            </div>
+            <div className="select-container">
+                <Select value={colorFormat} onChange={handleChange}>
+                    <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                    <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                    <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
+                </Select>
             </div>
         </header>
     )
--- src/Palette.js
+++ src/Palette.js
@@ -6,18 +6,26 @@ import "./Palette.css";
 function Palette(props) {
     // State of color level
     const [colorLevel, setColorLevel] = useState(500);
+    const [format, setFormat] = useState("hex");
+
     // Used by slider to change the color brightness level
     function changeLevel(newLevel) {
         setColorLevel(newLevel);
     };
+
+    // Change the color display format, HEX, RGB, RGBA
+    function changeColorFormat(val) {
+        setFormat(val)
+    }
+
     // Sets the background color for each box
     const colorBoxes = props.palette.colors[colorLevel].map(color => (
-        <Colorbox background={color.hex} name={color.name} />))
+        <Colorbox background={color[format]} name={color.name} />))
 
     return (
         <div className="Palette">
             {/*navbar here*/}
-            <Navbar colorLevel={colorLevel} changeLevel={changeLevel}/>
+            <Navbar colorLevel={colorLevel} changeLevel={changeLevel} handleFormatChange={changeColorFormat}/>
             <div className="Palette-colors">
             {/*color boxes*/}
                 {colorBoxes}
