c509e0731e0db97cd48c3582366203cd8ae2d01d
JamesRonderos <wakeru@gmail.com>
2020-12-19 17:41:03 +0900

    Added unsytled color slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Colorbox from "./Colorbox";
 import "./Palette.css";
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 function Palette(props) {
-
-    const colorBoxes = props.palette.colors.map(color => (<Colorbox background={color.color} name={color.name} />))
+    const {colors} = props.palette.colors; // for easier use later
+    // State of color level
+    const [colorLevel, setColorLevel] = useState(500);
+    // Used by slider to change the color brightness level
+    function changeLevel(newLevel) {
+        setColorLevel(newLevel);
+    };
+    // Sets the background color for each box
+    const colorBoxes = colors[colorLevel].map(color => (
+        <Colorbox background={color.hex} name={color.name} />))
 
     return (
         <div className="Palette">
+            // Slider at top to change color brightness
+            <Slider defaultValue={colorLevel} min={100} max={900} step={100} onAfterChange={changeLevel}/>
             {/*navbar here*/}
             <div className="Palette-colors">
             {/*color boxes*/}
