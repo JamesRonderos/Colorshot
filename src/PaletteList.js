472bb8ec3091563291d726f0d52e30189e63dbde
JamesRonderos <wakeru@gmail.com>
2020-12-31 17:14:29 +0900

    Started styling miniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MiniPalette from "./MiniPalette";
 import { Link } from "react-router-dom";
 
 function PaletteList(props) {
@@ -7,14 +8,17 @@ function PaletteList(props) {
 
     return (
         <div>
+            <MiniPalette />
             <h1>React Colors</h1>
             {palettes.map(palette => (
-                <p>
-                <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                </p>
+            <MiniPalette {...palette}/>
                 ))}
         </div>
     );
 }
 
 export default PaletteList;
+
+// <p>
+//     <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+// </p>
