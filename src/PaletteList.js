2ba44f681847d7e65199c1a28977e735336ca19f
JamesRonderos <wakeru@gmail.com>
2021-01-01 21:02:28 +0900

    miniPalletes now link to their full versions

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -38,12 +38,16 @@ function PaletteList(props) {
 
     const { palettes, classes } = props;
 
+    function goToPalette(id) {
+        props.history.push(`/palette/${id}`)
+    };
+
     return (
         <div className={classes.root}>
             <div className={classes.container}>
                 <nav className={classes.nav}><h1>React Colors</h1></nav>
                 <div className={classes.palettes}>{palettes.map(palette => (
-                    <MiniPalette {...palette}/>
+                    <MiniPalette {...palette} handleClick={() => goToPalette(palette.id)}/>
                 ))}
                 </div>
             </div>
