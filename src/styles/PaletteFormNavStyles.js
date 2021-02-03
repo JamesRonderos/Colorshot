67c1ad59ae879d94fbb9a6b6825d8d151e9abbf5
JamesRonderos <wakeru@gmail.com>
2021-02-03 23:51:53 +0900

    Changed styles of form

--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -15,6 +15,9 @@ const styles = theme => ({
         alignItems: "center",
         height: "64px"
     },
+    hide: {
+        display: 'none',
+    },
     appBarShift: {
         width: `calc(100% - ${drawerWidth}px)`,
         marginLeft: drawerWidth,
