028fa24fd145452078b7f6d99940efcf904476ed
JamesRonderos <wakeru@gmail.com>
2021-02-04 20:43:28 +0900

    Made ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -14,7 +14,7 @@ export default {
         },
         [sizes.down("lg")]: {
             width: "25%",
-            height: props => (props.showingFullPalette ? "20%" : "20%"),
+            height: props => (props.showingFullPalette ? "20%" : "33.333%"),
         },
         [sizes.down("md")]: {
             width: "50%",
@@ -116,6 +116,9 @@ export default {
             marginBottom: "0",
             padding: "1rem",
             textTransform: "uppercase",
+            [sizes.down("xs")]: {
+                fontSize: "6rem",
+            },
         },
         "& p": {
             fontSize: "2rem",
